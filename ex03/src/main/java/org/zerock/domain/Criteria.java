16a7a4982863e791231f2233f2333562467fc979
KimJiHoon <rldnssl1@naver.com>
2021-10-20 23:46:52 +0900

    Ajax 구현을 위한 기존 ex02 소스 동기화

---
+++ ex03/src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,32 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+	
+	//페이징 처리를 위한 변수
+	private int pageNum;
+	private int amount;
+	
+	
+	//입력값을 위한 변수
+	private String type;
+	private String keyword;
+	
+	public Criteria() {
+		this(1,10,"ALL","");
+		System.out.println("생성자호출==============");
+	}
+	
+	public Criteria(int pageNum , int amount, String type, String keyword) {
+		this.pageNum = pageNum;
+		this.amount  = amount;
+		this.type 	 = type;
+		this.keyword = keyword;
+	}
+}
