3d262edc9f96289e822af20d4bc6c9a1faeccc7d
KimJiHoon <rldnssl1@naver.com>
2021-10-25 23:43:02 +0900

    /pages/{bno}/{page} GetMapping commit

--- ex03/src/main/java/org/zerock/controller/ReplyController.java
+++ ex03/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,12 +1,17 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -38,4 +43,29 @@ public class ReplyController {
 		}
 
 	}
+	
+	
+
+	 @GetMapping(value = "/pages/{bno}/{page}", 
+			 produces = {
+					 MediaType.APPLICATION_XML_VALUE,
+					 MediaType.APPLICATION_JSON_UTF8_VALUE })
+	 public ResponseEntity<List<ReplyVO>> getList(
+			 @PathVariable("page") int page,
+			 @PathVariable("bno") Long bno) {
+	
+		 
+		 log.info("getList.................");
+		 Criteria cri = new Criteria();
+		 log.info(cri);
+	
+	 return new ResponseEntity<List<ReplyVO>>(service.getList(cri, bno), HttpStatus.OK);
+	 }
+	
+	
+	
+	
+	
+	
+	
 }
