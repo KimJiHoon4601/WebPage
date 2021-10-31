8b8e1eb33b2a067e4111df3ead0ba55026b43468
KimJiHoon <rldnssl1@naver.com>
2021-10-31 23:22:29 +0900

    정수형 변수 , 소수형 변수 , 논리타입 test

--- chap01/src/sec03/exam01/Hello.java
+++ chap01/src/sec03/exam01/Hello.java
@@ -10,6 +10,20 @@ public class Hello {
 		int value = 30;
 		int result = value + 10;
 		System.out.println(result);
+		
+		
+		long a = 10;
+		long bb = 1L;
+		
+		
+		char cc = 65;
+		System.out.println(((Object)a).getClass().getSimpleName());
+		System.out.println(((Object)bb).getClass().getSimpleName());
+		System.out.println(((Object)cc).getClass().getSimpleName());
+		
+		System.out.println("sdff"+cc);
+	
+	
 	}
 
 }
---
+++ chap01/src/sec03/exam01/basicType.java
@@ -0,0 +1,21 @@
+package sec03.exam01;
+
+public class basicType {
+	public static void main(String[] args) {
+		
+		char c = 65;
+		int i = 65;
+		
+		System.out.println(c);  //A 출력
+		System.out.println(i);  //65 출력
+		
+		long l = 1000000000000L;
+		
+		String s = "나는 \"자바\"를 매우 좋아한다";
+		System.out.println(s);     //나는 "자바"를 매우 좋아한다
+
+		float f= 3.14;
+		float f2 = 3.14F;
+		
+	}
+}
