16015ff28961336c58566f4b22643a6d57702676
KimJiHoon <rldnssl1@naver.com>
2021-10-31 21:50:55 +0900

    변수 초기화 오류 변수는 선언만 할 경우에는 아직 메모리 번지 정보를 가지고 있지 않으므로, 메모리 값을 읽을 수 없다.

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Java");
+		
+		//변수 초기화 오류
+		int value;
+		int result = valye + 10;
+		System.out.println(result);
+	}
+
+}
